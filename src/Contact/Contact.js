0e7b85b5f5fbd0dc5e7bd89ec2686b5b30943f89
Amanda <amanda.tjan@gmail.com>
2018-09-02 21:37:34 +0200

    Add in styling ;

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,6 +5,10 @@ const Contact = () => {
   return (
     <div className='Contact'>
       <h2>Contact</h2>
+      <div className='contact-info-cont'>
+        <p>Give me yo phone number</p>
+        <p>Give me yo email</p>
+      </div>
     </div>
   )
 }
--- src/Diary/Diary.js
+++ src/Diary/Diary.js
@@ -4,7 +4,11 @@ import './Diary.css';
 const Diary = () => {
   return (
     <div className='Diary'>
-      image here
+      <h2>Diary</h2>
+      <div className='info-content'>
+        <p>Tattooed seitan mlkshk, hammock woke 90's pitchfork polaroid. Schlitz 8-bit brooklyn, occupy hashtag flannel adaptogen venmo portland mumblecore 3 wolf moon swag bitters. Woke chicharrones poke, cronut echo park hot chicken helvetica vice fashion axe blog brooklyn stumptown banh mi shabby chic. Normcore bespoke plaid helvetica, subway tile single-origin coffee photo booth gochujang aesthetic poke fingerstache. Occupy affogato plaid, taxidermy neutra tilde YOLO tofu deep v PBR&B yr cold-pressed.</p>
+        <p>Mixtape keytar succulents pug pitchfork prism. Cray williamsburg chartreuse godard slow-carb kitsch. Umami disrupt vape biodiesel. Flannel fixie brunch, hammock tilde woke godard aesthetic semiotics. Intelligentsia YOLO cold-pressed enamel pin. Raclette chia crucifix offal enamel pin twee gochujang hot chicken leggings thundercats poke synth swag marfa williamsburg.</p>
+      </div>
     </div>
   )
 }
